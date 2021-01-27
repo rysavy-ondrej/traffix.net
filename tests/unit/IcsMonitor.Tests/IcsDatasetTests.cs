667966710415922eddb532850e3d7f73d2de6f74
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2021-01-27 21:07:34 +0100

    Update IcsDatasetTests.cs

--- tests/unit/IcsMonitor.Tests/IcsDatasetTests.cs
+++ tests/unit/IcsMonitor.Tests/IcsDatasetTests.cs
@@ -28,6 +28,8 @@ namespace IcsMonitor.Tests
             var dataset = ctx.ComputeModbusDataset(@"C:\Users\user\Captures\sorting_station_ver2.pcap", TimeSpan.FromSeconds(180));
             var records = dataset.ConversationTables.SelectMany(x => x.AsEnumerable());
             var dataframe =records.ToDataFrame();
+            var schema = dataframe.Schema;
+            Console.WriteLine($"Schema={schema}");
             Console.WriteLine($"DataView rows = {dataframe.Rows.Count()}");
             Console.WriteLine($"{dataframe.ToString()}");
         }
