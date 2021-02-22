42eaeebe42d16231e0f0f8795d7b50c6cc41ad60
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2021-02-22 12:11:19 +0100

    Update perf test.

--- tests/perf/FasterConversationTable.Perf/Program.cs
+++ tests/perf/FasterConversationTable.Perf/Program.cs
@@ -1,69 +1,45 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 using System;
 using System.IO;
+using Traffix.Providers.PcapFile;
 
 namespace FasterConversationTable.Perf
 {
-    class Program
+    [SimpleJob(RunStrategy.Monitoring, targetCount: 10)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class IngestPacketTraceBenchmark
     {
 
-        public class IngestPacketTraceBenchmark
+        [Params(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.pcap", @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151021.pcap", @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.pcap")]
+        public string dataset;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            private byte[] data;
-
-            [Params(5)]
-            public int N;
-
-            [GlobalSetup]
-            public void Setup()
-            {
-            }
-
-
-            [Benchmark]
-            public void LoadTable1()
-            {
-                CreateTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.nfx", 300000, @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.pcap");
-                DeleteTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.nfx");
-            }
-            [Benchmark]
-            public void LoadTable2()
-            {
-                CreateTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151021.nfx", 1500000, @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.pcap");
-                DeleteTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151021.nfx");
-            }
-            [Benchmark]
-            public void LoadTable3()
-            {
-                CreateTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.nfx", 2500000, @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.pcap");
-                DeleteTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.nfx");
-            }
-
-            [Benchmark]
-            public void ReadExistingTable1() => OpenTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.nfx");
-            [Benchmark]
-            public void ReadExistingTable2() => OpenTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151021.nfx");
-            [Benchmark]
-            public void ReadExistingTable3() => OpenTable(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.nfx");
-
-
-            public static Traffix.Storage.Faster.FasterConversationTable OpenTable(string tablePath)
-            {
-                var table = Traffix.Storage.Faster.FasterConversationTable.Open(tablePath);
-                return table;
-            }
-            public static Traffix.Storage.Faster.FasterConversationTable CreateTable(string tablePath, long framesCount, string sourcePcap)
-            {
-                var table = Traffix.Storage.Faster.FasterConversationTable.Create(tablePath, framesCount);
-                return table;
-            }
-            public static void DeleteTable(string tablePath)
-            {
-                Directory.Delete(tablePath, true);
-            }
         }
 
+        int loadTableRun = 0;
+        [Benchmark]
+        public void LoadTable()
+        {
+            loadTableRun++;
+            using var flowTable = Traffix.Storage.Faster.FasterConversationTable.Create($"{dataset}.{loadTableRun}", 3000000);
+            using (var loader = flowTable.GetStreamer())
+            using (var pcapReader = new SharpPcapReader(dataset))
+            {
+                while (pcapReader.GetNextFrame(out var rawFrame))
+                {
+                    loader.AddFrame(rawFrame);
+                }
+                loader.Close();
+            }
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
             var summary = BenchmarkRunner.Run<IngestPacketTraceBenchmark>();
