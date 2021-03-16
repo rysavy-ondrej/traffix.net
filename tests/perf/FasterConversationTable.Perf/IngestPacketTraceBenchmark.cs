947782f6d651b3b8f121f0f8bd2874c3d7e0b0b6
Ondřej Ryšavý <rysavy@fit.vutbr.cz>
2021-03-16 10:07:19 +0100

    Add perf test

---
+++ tests/perf/FasterConversationTable.Perf/IngestPacketTraceBenchmark.cs
@@ -0,0 +1,88 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System;
+using System.Linq;
+using Traffix.Providers.PcapFile;
+using Traffix.Storage.Faster;
+using Traffix.Processors;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace FasterConversationTablePerf
+{
+
+    [SimpleJob(RunStrategy.Monitoring, targetCount: 5)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class IngestPacketTraceBenchmark
+    {
+
+        [Params(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.pcap", @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151021.pcap", @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.pcap")]
+        public string dataset;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _conversationTable = FasterConversationTable.Create($"{dataset}.{loadTableRun}", 3000000);
+            using (var loader = _conversationTable.GetStreamer())
+            using (var pcapReader = new SharpPcapReader(dataset))
+            {
+                while (pcapReader.GetNextFrame(out var rawFrame))
+                {
+                    loader.AddFrame(rawFrame);
+                }
+                loader.Close();
+            }
+        }
+        Traffix.Storage.Faster.FasterConversationTable _conversationTable;
+
+        int loadTableRun = 0;
+        [Benchmark]
+        public void LoadTable()
+        {
+            loadTableRun++;
+            using var conversationTable = FasterConversationTable.Create($"{dataset}.{loadTableRun}", 3000000);
+            using (var loader = conversationTable.GetStreamer())
+            using (var pcapReader = new SharpPcapReader(dataset))
+            {
+                while (pcapReader.GetNextFrame(out var rawFrame))
+                {
+                    loader.AddFrame(rawFrame);
+                }
+                loader.Close();
+            }
+        }
+
+        [Benchmark]
+        public void ExportPackets()
+        {
+            var packets = _conversationTable.ProcessFrames(_conversationTable.FrameKeys, new FasterConversationTable.PacketFrameProcessor());
+            packets.Count();
+        }
+        [Benchmark]
+        public void ExportConversations()
+        {
+            var conversations = _conversationTable.ProcessConversations(_conversationTable.ConversationKeys, new FasterConversationTable.PacketConversationProcessor());
+            conversations.Count();
+        }
+        [Benchmark]
+        public void GroupWindowedConversations()
+        {
+            var start = DateTimeOffset.FromUnixTimeSeconds(_conversationTable.FrameKeys.First().Epoch);
+            var end = DateTimeOffset.FromUnixTimeSeconds(_conversationTable.FrameKeys.Last().Epoch);
+            var span = (end - start) / 100;
+            var windows = _conversationTable.Conversations.GroupByWindow(start.DateTime, span);
+            windows.Count();
+        }
+        [Benchmark]
+        public void ExportWindowedConversations()
+        {
+            var start = DateTimeOffset.FromUnixTimeSeconds(_conversationTable.FrameKeys.First().Epoch);
+            var end = DateTimeOffset.FromUnixTimeSeconds(_conversationTable.FrameKeys.Last().Epoch);
+            var span = (end - start) / 100;
+            var windows = _conversationTable.Conversations.GroupByWindow(start.DateTime, span);
+            var conversations = windows.SelectMany(conversationKeys =>
+                _conversationTable.ProcessConversations(conversationKeys, new FasterConversationTable.PacketConversationProcessor().ApplyToWindow(start.DateTime, span)));
+            conversations.Count();
+        }
+    }
+}
---
+++ tests/perf/FasterConversationTable.Perf/IngestPacketTraceBenchmarkObservable.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System;
+using System.Linq;
+using Traffix.Providers.PcapFile;
+using Traffix.Core.Observable;
+using Traffix.Core.Flows;
+using PacketDotNet;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace FasterConversationTablePerf
+{
+    [SimpleJob(RunStrategy.Monitoring, targetCount: 5)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class IngestPacketTraceBenchmarkObservable
+    {
+        [Params(@"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151020.pcap", @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151021.pcap", @"D:\Captures\ecbs21paper.datasets\4SICS-GeekLounge-151022.pcap")]
+        public string dataset;
+
+
+        private (long Ticks, Packet Packet) GetPacket(RawFrame arg)
+        {
+            return (arg.Ticks, Packet.ParsePacket(arg.LinkLayer, arg.Data));
+        }
+        private async Task<string> FlowProcessor(FlowKey flowKey, IObservable<(long Ticks, Packet Packet)> packets)
+        {
+            var packetCount = 0;
+            var firstSeen = long.MaxValue;
+            var lastSeen = long.MinValue;
+            var octets = 0;
+            await packets.ForEachAsync(packet =>
+            {
+                packetCount++;
+                octets += packet.Packet.TotalPacketLength;
+                firstSeen = Math.Min(firstSeen, packet.Ticks);
+                lastSeen = Math.Max(lastSeen, packet.Ticks);
+            });
+            return $"  Flow {flowKey}: firstSeen={new DateTime(firstSeen)}, duration={new TimeSpan(lastSeen - firstSeen)}, packets={packetCount}, octets={octets}";
+        }
+
+        [Benchmark]
+        public async Task ExportWindowedConversationsObservable()
+        {
+            var observable = SharpPcapReader.CreateObservable(dataset).Select(GetPacket);
+            // get windows of flows:
+            var wins = observable.TimeWindow(t => t.Ticks, TimeSpan.FromMinutes(5)).Select(o => o.GroupBy(f => f.Packet.GetFlowKey()));
+            var windowNumber = 0;
+            await wins.ForEachAsync(async win =>
+            {
+                Console.Write($"Window {++windowNumber}:  ");
+                await win.ApplyFlowProcessor(FlowProcessor).ForEachAsync(flowStr =>
+                {
+                    Console.Write('.');
+                });
+                Console.WriteLine();
+            });
+        }
+    }
+}
